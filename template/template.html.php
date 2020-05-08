80495b53185b74ee44bbffc682abc3b5ca9adc76
RyanVanderZanden1 <65002727+RyanVanderZanden1@users.noreply.github.com>
2020-05-07 21:40:12 -0700

    Add files via upload

---
+++ template/template.html.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+File Name: template.html
+Date: 05/8/20
+Programmer: Ryan VanderZanden
+-->
+<head>
+    
+<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+<meta charset="UTF-8">
+<link href="css/nav.css" rel="stylesheet" type="text/css">
+<link href="css/style.less" rel="stylesheet/less" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+
+<title>Ace in the Hole: Template</title>
+
+</head>
+
+<body>
+<div class="content-wrapper">
+<!-- navigation and hamburger menu -->
+    
+<header class="header">
+    
+    <input class="menu-btn" type="checkbox" id="menu-btn"/>
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    
+    <h1>Ace in the Hole</h1>
+    
+    <ul class="menu">
+        <li><a href="#" target="_self">Home</a></li>
+        <li><a href="#" target="_self">Packet Pick Up</a></li>
+        <li><a href="#" target="_self">Register</a></li>
+        <li><a href="#" target="_self">Contact</a></li>
+    </ul>
+</header>
+    
+<!-- container for tablet and desktop -->
+<div id="container">     
+    
+<main>   
+    
+<h2>Lorem Ipsum</h2>
+    
+<p>Integer ligula erat, accumsan vitae dui eget, pharetra efficitur ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut tristique, augue a cursus bibendum, nulla ante cursus dui, sed luctus neque leo eget lectus. Fusce eget lacus id velit viverra ornare sed in nulla. Cras vitae porttitor neque, vitae efficitur erat. Duis egestas viverra pretium. Etiam cursus sit amet orci imperdiet suscipit. Donec vitae velit nec dui interdum tincidunt nec sit amet odio. Integer malesuada nisi id nulla convallis sodales. Fusce vel urna vulputate, congue sem ac, dapibus ligula. Nam id blandit justo. </p>
+
+<p>Nam feugiat efficitur nisl eu ullamcorper. In ex quam, imperdiet et nisi nec, blandit fringilla augue. Maecenas sit amet mi placerat, sodales arcu nec, ornare arcu. Nam nibh nunc, vestibulum non risus eu, efficitur tempor ipsum. </p>    
+    
+</main>
+
+<footer>
+ 
+<p>Images are licensed under the Creative Commons CC0 from <a href="https://www.pexels.com">https://pixabay.com</a>.</p>
+<?php include 'footer.php';?>
+    
+<!-- social media icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" class="fa fa-facebook"></a>
+<a href="https://twitter.com/pcccas222" class="fa fa-twitter"></a>
+    
+</footer>
+    </div>
+</div>    
+
+</body>
+    
